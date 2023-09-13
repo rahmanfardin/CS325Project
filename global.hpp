c0a7f08b46ec7e84a1612fed406d8ca9134a0c6b
rahmanfardin <mfardinr@gmail.com>
2023-09-13 13:44:31 +0600

    all file done

---
+++ global.hpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+bool isAvailable(vector<int> array, int key)
+{
+    for (int i = 0; i < array.size(); i++)
+    {
+        if (array[i] == key)
+            return true;
+    }
+    return false;
+}
+void makeArray(vector<int> array, int pageSize)
+{
+    for (int i = 0; i < pageSize; i++)
+        array.at(i) = -1;
+}
+void printArray(vector<int> array, int pageSize)
+{
+    for (int i = 0; i < pageSize; i++)
+    {
+        cout << "\t\t f " << i << " : " << array[i];
+    }
+}
+
+int findMIN(vector<int> givenArray, int Size)
+{
+    int min = givenArray[0], index = 0;
+    for (int i = 0; i < Size; i++)
+    {
+        if (min > givenArray[i])
+        {
+            min = givenArray[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+int findIndex(vector<int> givenArray, int Size, int key)
+{
+    for (int i = Size; i >= 0; i--)
+    {
+        if (givenArray[i] == key)
+            return i;
+    }
+    return INT_MAX;
+}
+
+vector<int> leastIdx(vector<int> array, int pageSize, vector<int> givenArray, int Size)
+{
+    vector<int> index(pageSize, -1);
+    for (int i = 0; i < pageSize; i++)
+    {
+        int j = findIndex(givenArray, Size, array[i]);
+        index[i] = j;
+    }
+    return index;
+}
+
+int lru(vector<int> array, int pageSize, vector<int> givenArray, int Size)
+{
+    int fault = 0, count = 0;
+    makeArray(array, pageSize);
+
+    int index = 0;
+    for (int i = 0; i < Size; i++)
+    {
+        if (count >= pageSize)
+        {
+            vector<int> lindex = leastIdx(array, pageSize, givenArray, i+1);
+            index = findMIN(lindex, pageSize);
+        }
+
+        int value = givenArray[i];
+        if (!isAvailable(array, value))
+        {
+            array[index] = value;
+            if (count >= pageSize)
+                fault++;
+            count++;
+            index++;
+        }
+
+        cout << "value-->" << value;
+        printArray(array, pageSize);
+        cout << endl;
+    }
+
+    return fault;
+}
+
+int fifo(vector<int> array, int pageSize, vector<int> givenArray, int Size)
+{
+    int fault = 0, top = 0, count = 0;
+    makeArray(array, pageSize);
+
+    for (int i = 0; i < Size; i++)
+    {
+        if (top == pageSize)
+        {
+            count = 1;
+            top = 0;
+        }
+        int value = givenArray[i];
+        if (!isAvailable(array, value))
+        {
+            array[top] = value;
+            if (count != 0)
+                fault++;
+            top++;
+        }
+        cout << "value-->" << value;
+        printArray(array, pageSize);
+        cout << endl;
+    }
+    return fault;
+}
+
+vector<int> opIdx(vector<int> array, int pageSize, vector<int> givenArray, int Size)
+{
+    vector<int> index(pageSize, -1);
+    for (int i = 0; i < pageSize; i++)
+    {
+        int j = findIndex(givenArray, Size, array[i]);
+        index[i] = j;
+    }
+    return index;
+}
+int findMAX(vector<int> givenArray, int Size){
+    int max = givenArray[0], index = 0;
+    for (int i = 0; i < Size; i++)
+    {
+        if (max < givenArray[i])
+        {
+            max = givenArray[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+int optimal(vector<int> array, int pageSize, vector<int>givenArray, int Size){
+    int fault = 0, count = 0;
+    makeArray(array, pageSize);
+    int index = 0;
+    for (int i = 0; i < Size; i++)
+    {
+        if (count >= pageSize)
+        {
+            vector<int> lindex = opIdx(array, pageSize, givenArray, Size);
+            index = findMAX(lindex, pageSize);
+        }
+
+        int value = givenArray[i];
+        if (!isAvailable(array, value))
+        {
+            array[index] = value;
+            if (count >= pageSize)
+                fault++;
+            count++;
+            index++;
+        }
+
+        cout << "value-->" << value;
+        printArray(array, pageSize);
+        cout << endl;
+    }
+
+    return fault;
+}
---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include "global.hpp"
+
+int main()
+{
+    int Size, pageSize = 0;
+    bool run = true;
+
+    cout << "enter the number of elements: ";
+    cin >> Size;
+
+    vector<int> givenArray;
+
+    cout << "start entering the values: " << endl;
+    for (int i = 0; i < Size; i++)
+    {
+        int value = -1;
+        cin >> value;
+        givenArray.push_back(value);
+    }
+
+    while (run)
+    {
+        int choice = -1;
+        cout << "1. FIFO" << endl
+             << "2. LRU" << endl
+             << "3. OPTIMAL" << endl
+             << "9. EXIT"<< endl;
+
+        cout << "make your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+        {
+            cout << "enter the page count: ";
+            cin >> pageSize;
+            vector<int> array(pageSize, -1);
+            cout << "=========================================================================================" << endl
+                 << "FIFO: " << endl;
+            int fifoFault = fifo(array, pageSize, givenArray, Size);
+            cout << "Fault: " << fifoFault << endl
+                 << "=========================================================================================" << endl;
+            break;
+        }
+        case 2:
+        {
+            cout << "enter the page count: ";
+            cin >> pageSize;
+            vector<int> array(pageSize, -1);
+            cout << "=========================================================================================" << endl
+                 << "LRU: " << endl;
+            int lruFault = lru(array, pageSize, givenArray, Size);
+            cout << "Fault: " << lruFault << endl
+                 << "=========================================================================================" << endl;
+            break;
+        }
+        case 3:
+        {
+            cout << "enter the page count: ";
+            cin >> pageSize;
+            vector<int> array(pageSize, -1);
+            cout << "=========================================================================================" << endl
+                 << "LRU: " << endl;
+            int optFault = optimal(array, pageSize, givenArray, Size);
+            cout << "Fault: " << optFault << endl
+                 << "=========================================================================================" << endl;
+            break;
+        }
+        case 9:
+            run = false;
+            break;
+        default:
+            break;
+        }
+    }
+}
